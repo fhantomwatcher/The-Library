d50dfc2f332b496cf6cc316f881d5e2917da9f7a
sajidifty2-create <sajidifty2@gmail.com>
2026-09-15 14:42:20 +0600

    Add Member's library management module

---
+++ Member/MyFines.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Member_data
+{
+    partial class MyFines
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnBack = new System.Windows.Forms.Button();
+            this.dgvFines = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFines)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(82, 303);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(154, 44);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Show";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(333, 303);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(127, 44);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // dgvFines
+            // 
+            this.dgvFines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFines.Location = new System.Drawing.Point(31, 12);
+            this.dgvFines.Name = "dgvFines";
+            this.dgvFines.RowHeadersWidth = 51;
+            this.dgvFines.RowTemplate.Height = 24;
+            this.dgvFines.Size = new System.Drawing.Size(716, 253);
+            this.dgvFines.TabIndex = 3;
+            // 
+            // MyFines
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFines);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "MyFines";
+            this.Text = "MyFines";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFines)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView dgvFines;
+    }
+}
---
+++ Member/Reservations.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Member_data
+{
+    public partial class Reservations : Form
+    {
+        public Reservations()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Form1 dashboard = new Form1();
+            dashboard.Show();
+            this.Hide();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(
+                @"Data Source=.\SQLEXPRESS;Initial Catalog=LMSDb;Integrated Security=True;TrustServerCertificate=True");
+
+            string query = "SELECT * FROM BookCopies WHERE Status = 'available'";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgvReservations.DataSource = dt;
+        }
+
+        private void btnReserve_Click(object sender, EventArgs e)
+        {
+            if (txtBookID.Text == "")
+            {
+                MessageBox.Show("Please enter Book ID first!");
+                return;
+            }
+
+            string bookID = txtBookID.Text;
+
+            SqlConnection con = new SqlConnection(
+                @"Data Source=.\SQLEXPRESS;Initial Catalog=LMSDb;Integrated Security=True;TrustServerCertificate=True");
+
+            string checkQuery = "SELECT COUNT(*) FROM BookCopies WHERE BookID = @BookID AND Status = 'available'";
+            SqlCommand checkCmd = new SqlCommand(checkQuery, con);
+            checkCmd.Parameters.AddWithValue("@BookID", bookID);
+
+            con.Open();
+            int count = (int)checkCmd.ExecuteScalar();
+
+            if (count == 0)
+            {
+                con.Close();
+                MessageBox.Show("Book not available for reservation.");
+                return;
+            }
+
+            string insertQuery = "INSERT INTO Reservations (BookID, ReservationDate, ExpiryDate, Status) " +
+                                  "VALUES (@BookID, GETDATE(), DATEADD(day, 7, GETDATE()), 'Pending')";
+            SqlCommand insertCmd = new SqlCommand(insertQuery, con);
+            insertCmd.Parameters.AddWithValue("@BookID", bookID);
+            insertCmd.ExecuteNonQuery();
+
+            string updateQuery = "UPDATE BookCopies SET Status = 'reserved' WHERE BookID = @BookID";
+            SqlCommand updateCmd = new SqlCommand(updateQuery, con);
+            updateCmd.Parameters.AddWithValue("@BookID", bookID);
+            updateCmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Book reserved successfully!");
+
+            btnRefresh_Click(sender, e);
+        }
+
+        private void btnCancelReservation_Click(object sender, EventArgs e)
+        {
+            if (txtBookID.Text == "")
+            {
+                MessageBox.Show("Please enter Book ID first!");
+                return;
+            }
+
+            string bookID = txtBookID.Text;
+
+            SqlConnection con = new SqlConnection(
+                @"Data Source=.\SQLEXPRESS;Initial Catalog=LMSDb;Integrated Security=True;TrustServerCertificate=True");
+
+            string deleteQuery = "DELETE FROM Reservations WHERE BookID = @BookID";
+            SqlCommand deleteCmd = new SqlCommand(deleteQuery, con);
+            deleteCmd.Parameters.AddWithValue("@BookID", bookID);
+
+            con.Open();
+            deleteCmd.ExecuteNonQuery();
+
+            string updateQuery = "UPDATE BookCopies SET Status = 'available' WHERE BookID = @BookID";
+            SqlCommand updateCmd = new SqlCommand(updateQuery, con);
+            updateCmd.Parameters.AddWithValue("@BookID", bookID);
+            updateCmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Reservation cancelled successfully!");
+
+            btnRefresh_Click(sender, e);
+        }
+    }
+}
